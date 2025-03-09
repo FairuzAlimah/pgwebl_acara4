ab2f9049ed2a9bca5dcfc7e23ff21f24778692c3
FairuzAlimah <fairuzalimahulayyahuda@mail.ugm.ac.id>
2025-03-09 14:27:45 +0700

    Leaflet Draw and Create Point

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create(
+            [
+                'name' => 'Huda',
+                'email' => 'fairuzalimah.uh@gmail.com',
+                'password' => bcrypt('admin123'),
+            ]
+        );
+        User::factory()->create(
+            [
+                'name' => 'Fay',
+                'email' => 'fairuzalimahulayyahuda@mail.ugm.ac.id',
+                'password' => bcrypt('admin123'),
+            ]
+        );
+    }
+}
---
+++ resources/views/table.blade.php
@@ -0,0 +1,48 @@
+@extends('layout.template')
+
+@section('content')
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>NIM</th>
+                <th>Kelas</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+
+                <td>1</td>
+                <td>Fay</td>
+                <td>514254</td>
+                <td>A</td>
+
+            </tr>
+            <tr>
+
+                <td>2</td>
+                <td>Huda</td>
+                <td>512544</td>
+                <td>B</td>
+
+            </tr>
+            <tr>
+
+                <td>1</td>
+                <td>Ulya</td>
+                <td>542541</td>
+                <td>A</td>
+
+            </tr>
+            <tr>
+
+                <td>1</td>
+                <td>Alim</td>
+                <td>512454</td>
+                <td>A</td>
+
+            </tr>
+        </tbody>
+    </table>
+@endsection
